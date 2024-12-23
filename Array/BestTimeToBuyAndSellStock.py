d8c92215be2edc0872c0d7a5a0318e7f8d561ba9
Valary Anguzuzu <anguzuzuvalary@gmail.com>
2024-12-23 17:37:49 +0300

    Added solution for Best Time to Buy and Sell Stock - Implemented a two-pointer solution - Updated README to reflect description

---
+++ Array/BestTimeToBuyAndSellStock.py
@@ -0,0 +1,59 @@
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+# You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+# Example 1:
+
+# Input: prices = [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+# Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+# Example 2:
+
+# Input: prices = [7,6,4,3,1]
+# Output: 0
+# Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+# Constraints:
+
+# 1 <= prices.length <= 105
+# 0 <= prices[i] <= 104
+
+#we always want to buy low and sell high
+#TWO POINTERS
+#profitable means prices[r] > prices[l]
+#profit is the difference
+#initialize max profit = 0
+#we want to keep updating profit so long as new profit we get > cur profit
+#iterate while r is < len(prices) - does not go out of bounds
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        l, r = 0, 1 #l= buy, r= sell
+        MaxProfit =  0
+        while r < len(prices):
+        #is it profitable?
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                MaxProfit = max(MaxProfit, profit)
+            else: #shift l all the way to r is would be the cur lowest price
+                l =r
+            r +=1 #either way move r forward
+        return MaxProfit
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.maxProfit([7, 1, 5, 3, 6, 4]))  # Output: 5
+    print(solution.maxProfit([7, 6, 4, 3, 1]))       # Output: 0
+
+
+
+
---
+++ Array/README.md
@@ -0,0 +1,4 @@
+# Best Time to Buy and Sell Stock
+# Difficulty: Easy  
+# Approach: Two Pointers  
+# Optimal Complexity: O(n) time, O(1) space 
